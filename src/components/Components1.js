d3508bef053ad53e87891dd3dcd03dd8600961d9
vidyavathi <vidyavathi15@gmail.com>
2022-06-23 10:25:00 +0530

    sass practicing

---
+++ src/components/Components1.js
@@ -0,0 +1,40 @@
+import React from 'react' 
+
+
+import "./mySass.css"
+
+
+
+
+function Components1() {
+  return (
+      <>
+      <div className="initial">
+      <h1 className='second-name' >Hello Style!</h1>
+    <p>Add a little style!.</p>
+
+
+      </div>
+      
+    
+      
+
+    
+
+        <div className="second-container">
+        <h1 className='second-name'>sass styles</h1>
+        <p>sass processor</p>
+
+        </div>
+  
+      
+        </>
+    
+
+
+
+  
+  )
+}
+
+export default Components1
---
+++ src/components/component2.js
@@ -0,0 +1,28 @@
+import React from 'react' 
+import "./text.css"
+
+function component2() {
+
+
+  return (
+
+
+
+
+    <div className="bg-container">
+        <h1 className="heading-main">Scss styles</h1>
+        <div className="inside-container">
+            <p className="title">overwritting Scss styles</p>
+        </div>
+
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default component2
